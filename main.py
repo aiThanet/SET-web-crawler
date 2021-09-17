ecdf693cc9ab09425cf78ee15838ff268ee76a4d
tsirichanyap <thanet.sirichanyaphong@agoda.com>
2021-09-17 16:10:08 +0700

    fix bug

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils.strategy import Strategy
 
 for remark in ['XD','XW', 'XR']:
-    strategy = Strategy(start_date = '19-9-2021', end_date = '10-9-2022', remark=remark')
+    strategy = Strategy(start_date = '19-9-2021', end_date = '10-9-2022', remark=remark)
     strategy.generate()
 
 
